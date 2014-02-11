0914161e689632127853ef1d6021a3be99fa62af
Craig Sharkie <craig.sharkie@gmail.com>
2014-02-12 00:35:52 +1100

    [DATA] PLAYERS JSON
    Added JSON file used to create the players in the interface. Simply
    update this to add new players and remove the staging content. This
    should be updated in unison with the players.css

---
+++ public/data/players.js
@@ -0,0 +1,64 @@
+var players = {
+	"lineup" :[
+/*		
+		{
+			"name": "New Player",
+			"handle": "newplayer3",
+			"photo": "newplayer.png",
+		},
+		{
+			"name": "New Player",
+			"handle": "newplayer2",
+			"photo": "newplayer.png",
+		},
+		{
+			"name": "New Player",
+			"handle": "newplayer1",
+			"photo": "newplayer.png",
+		},
+*/	{
+			"name": "New Player",
+			"handle": "newplayer0",
+			"photo": "newplayer.png",
+		},
+		{
+			"name": "Moo Moo",
+			"handle": "moomoo",
+			"photo": "moomoo.png",
+			"stats": {
+				"JavaScript": 5,
+				"Ninja": 5
+			}
+		},
+		{
+			"name": "Sharkie",
+			"handle": "sharkie",
+			"photo": "sharkie.png",
+			"stats": {
+				"JavaScript": 4,
+				"jQuery": 4.5,
+				"DOM": 4.5
+			}
+		},
+		{
+			"name": "Jared Wyles",
+			"handle": "rioter",
+			"photo": "rioter.png",
+			"stats": {
+				"JavaScript": 4.5,
+				"jQuery": 3,
+				"Adobe": 5
+			}
+		},
+		{
+			"name": "Jen Fong-Adwent",
+			"handle": "jenfongadwent",
+			"photo": "jenfongadwent.jpg",
+			"stats": {
+				"GIFs": 5,
+				"WebRTC": 4,
+				"Node": 3
+			}
+		},
+	]
+}
